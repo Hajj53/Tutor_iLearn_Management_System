451ea87be096553381c8879b48b38ad579416e09
Hajji Mussa <hajj53@outlook.com>
2024-12-03 20:24:14 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tutor_iLearn_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
